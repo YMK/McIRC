e3412f87c4fe3cd08e5b0b59d7421d98eb2db2df
Alistair McKinlay <github@10people.co.uk>
2018-03-16 21:45:08 +0000

    Remve unnecessary lines in commands index

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,13 +1,7 @@
-const nick = require("./nick");
-const pong = require("./pong");
-const ping = require("./ping");
-const privmsg = require("./privmsg");
-const user = require("./user");
-
 module.exports = [
-	nick,
-	ping,
-	pong,
-	privmsg,
-	user,
+	require("./nick"),
+	require("./ping"),
+	require("./pong"),
+	require("./privmsg"),
+	require("./user"),
 ];
