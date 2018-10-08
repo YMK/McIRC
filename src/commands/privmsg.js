b93ce0cd1a48f6908ef531c04a49636304b5ad33
Alistair McKinlay <github@10people.co.uk>
2018-10-08 16:08:32 +0100

    Implement better error checking in PRIVMSG

--- src/commands/privmsg.js
+++ src/commands/privmsg.js
@@ -3,16 +3,42 @@ const state = require("../state");
 
 module.exports = {
 	test: (command) => command === Message.Command.PRIVMSG,
-	run: function (client, toSend) {
+	run: function (client, recipientList) {
+		if (!recipientList) {
+			return client.send(Message.Builder()
+				.withCommand(Message.Command.ERR_NORECIPIENT)
+				.withParameter(Message.Command.PRIVMSG)
+				.withParameter("No recipient given")
+				.build());
+		}
+
 		let message = Array.from(arguments).slice(2).join(" ");
+		if (!message) {
+			return client.send(Message.Builder()
+				.withCommand(Message.Command.ERR_NOTEXTTOSEND)
+				.withParameter(Message.Command.PRIVMSG)
+				.withParameter("No text to send")
+				.build());
+		}
+
 		if (message.indexOf(":") === 0) {
 			message = message.slice(1);
 		}
-		console.log(`PRIVMSG to ${toSend} with ${message}`);
-		const to = state.get(toSend);
-		if (!to) {
-			return console.log(`${toSend} doesn't exist`);
-		}
-		to.sendMessage(client.user.nick, message);
+
+		recipientList.split(",").forEach((recipient) => {
+
+			const to = state.get(recipient);
+			if (!to) {
+				return client.send(Message.Builder()
+					.withCommand(Message.Command.ERR_NOSUCHNICK)
+					.withParameter(recipient)
+					.withParameter("No such nick/channel")
+					.build());
+			}
+
+			console.log(`PRIVMSG to ${recipient} with ${message}`);
+			to.sendMessage(client.user.nick, message);
+
+		});
 	}
 };
