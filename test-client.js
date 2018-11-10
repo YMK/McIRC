191c14a7c5e840b52e1a88662b74581433a3e78f
Al McKinlay <github@10people.co.uk>
2018-11-10 13:18:00 +0000

    Fix test client parsing (#31)
    * Set up data listener before initial writing to socket
    
    * Read lines ready from server individually

--- test-client.js
+++ test-client.js
@@ -8,18 +8,24 @@ const rl = readline.createInterface({input: process.stdin, output: process.stdou
 const username = process.argv.length > 2 ? process.argv[2] : "test";
 client.setEncoding("utf8");
 
+client.on("data", (data) => {
+	let lines = data.toString().split(/\r\n/);
+	if (lines[lines.length - 1] === "") {
+		lines = lines.slice(0, lines.length - 1);
+	}
+	lines.forEach((line) => {
+		console.log(`Raw Received: ${line}`);
+		if (data.startsWith("PING")) {
+			console.log("Raw sending: PONG");
+			client.write("PONG\r\n")
+		}
+	});
+});
+
 client.write(`NICK ${username}\r\n`);
 client.write(`USER ${username} localhost * :Real name of user\r\n`);
-
-
-client.on("data", (data) => {
-	console.log(data);
-	if (data.startsWith("PING")) {
-		client.write("PONG\r\n");
-	}
-});
 
 rl.on("line", (line) => {
 	console.log(`Raw sending: ${line}`);
 	client.write(`${line}\r\n`);
-})
+});
