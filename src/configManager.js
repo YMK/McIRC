a802de3a330e95521e9c8af5150b1928e4b4920e
Al McKinlay <github@10people.co.uk>
2019-03-06 16:30:03 +0000

    Separate cli from the server itself, and allow config to be passed in (#79)
    Fixes #63

--- src/configManager.js
+++ src/configManager.js
@@ -1,18 +1,26 @@
 
-const defaultconfig = require("../config.default.json");
+const defaultConfig = require("../config.default.json");
 const CONFIG_FILE_NAME = "../config.json";
-let config = {};
-try {
-    config = require(CONFIG_FILE_NAME);
-} catch (e) {
-    // Log error that there is no config and we'll just be using the default
+const config = {
+    ...defaultConfig
 }
 
-const mergedConfig = {
-    ...defaultconfig,
-    ...config
-};
+const addConfigFromFile = () => {
+    let fileConfig;
+    try {
+        fileConfig = require(CONFIG_FILE_NAME);
+    } catch (e) {
+        // Log error that there is no config and we'll just be using the default
+    }
+    Object.assign(config, fileConfig);
+}
+
+const addConfig = (newConfig) => {
+    Object.assign(config, newConfig);
+}
 
 module.exports = {
-    config: mergedConfig
+    config,
+    addConfigFromFile,
+    addConfig
 };
